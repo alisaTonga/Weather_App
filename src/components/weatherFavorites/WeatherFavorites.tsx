1968e0bedc134ee174d89bea49ebbaaf4693243d
alisaTonga <tongalise@gmail.com>
2025-04-25 22:39:48 +0200

    fixed card adaptive styles

--- src/components/weatherFavorites/WeatherFavorites.tsx
+++ src/components/weatherFavorites/WeatherFavorites.tsx
@@ -1,34 +1,35 @@
 import { removeFavorite } from "../../features/weather/weatherSlice";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import WeatherCard from "../weatherCard/WeatherCard";
-import { getWeather } from '../../features/weather/weatherAction';
+import { getWeather } from "../../features/weather/weatherAction";
+import style from "./weatherFavorites.module.css";
 
 export default function WeatherFavorites() {
-    const dispatch = useAppDispatch();
-    const favorites = useAppSelector(state => state.weathers.favorites);
+  const dispatch = useAppDispatch();
+  const favorites = useAppSelector((state) => state.weathers.favorites);
 
-    const deleteCard = (id: number) => {
-        dispatch(removeFavorite(id));
-    };
+  const deleteCard = (id: number) => {
+    dispatch(removeFavorite(id));
+  };
 
-    return (
-        <div>
-        {favorites.length > 0 && (
-            <div>
-            {favorites.map((el) => (
-                <WeatherCard
-                key={el.id}
-                isNewCard={false}
-                favorites={favorites}
-                del={deleteCard}
-                city={el.name}
-                temp={Math.floor(el.main.temp - 273.15)}
-                image={`http://openweathermap.org/img/w/${el.weather[0].icon}.png`}
-                id={el.id}
-                />
-            ))}
-            </div>
-        )}
+  return (
+    <div className={style.favoritesContainer}>
+      {favorites.length > 0 && (
+        <div className={style.cardWrapper}>
+          {favorites.map((el) => (
+            <WeatherCard
+              key={el.id}
+              isNewCard={false}
+              favorites={favorites}
+              del={deleteCard}
+              city={el.name}
+              temp={Math.floor(el.main.temp - 273.15)}
+              image={`http://openweathermap.org/img/w/${el.weather[0].icon}.png`}
+              id={el.id}
+            />
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 }
